f4204a59620d4400388372e649cf1d531afaa5b2
valegaru <valegaru2003@gmail.com>
2024-03-01 12:43:34 -0600

    abuelo copia para segunda manera de usar data

--- prueba29feb/public/src/indexAbuelo copy.js
+++ prueba29feb/public/src/indexAbuelo copy.js
@@ -4,47 +4,24 @@ import * as components from './components/indexPadre.js';
 import { dataCards } from './components/data/dataCards.js';
 //otro componente que es el abuelo pero es un contenedor gigante de todo
 class AppContainer extends HTMLElement {
-	cards = [];
-
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
-
-		//dentro del constructor
-		//esto va a renderizar todo el data segun el componente DataCard que creamos en ComponenteDataCard.js
-		dataCards.forEach((element) => {
-			//creo el componente
-			const card = this.ownerDocument.createElement('data-card');
-			//le asigno una clase
-			card.className = 'Mi etiqueta';
-			//se van asignando las propiedades que deben aparecer en la card
-			card.setAttribute('name', element.name);
-			card.setAttribute('gender', element.gender);
-			card.setAttribute('house', element.house);
-			card.setAttribute('image', element.image);
-			//se va guardando cada carta en card de arribita del constructor
-			this.cards.push();
-		});
+};
 	}
 
-	connectedCallback() {
+	connectedCallback(); {
 		this.render();
 	}
 
-	render() {
+	render(); {
 		this.shadowRoot.innerHTML = `
           <link rel='stylesheet' href='./src/indexAbuelo.css'></link>
           <section>
           <nombre-en-etiqueta atributo="hola"></nombre-en-etiqueta>
           <nombre-en-etiqueta atributo="adios"></nombre-en-etiqueta>
  </section>
-
       `;
-
-		//para finalizar la manera larga de renderizar cada carta
-		this.cards.forEach((myCard) => {
-			this.shadowRoot.appendChild(myCard);
-		});
 
 		//manera corta de renderizar las cartas, solo se necesita esto
 		dataCards.forEach((element) => {
